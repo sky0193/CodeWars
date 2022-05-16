3a902efdb9e57947e66c59c044d712d2cf2a51bc
Lena Bykov <lena.bykov@aristech.de>
2022-05-16 12:14:14 +0200

    new: add 2 new tasks

---
+++ python_wars/calculating_with_functions.py
@@ -0,0 +1,58 @@
+﻿"""
+This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+
+seven(times(five())) # must return 35
+four(plus(nine())) # must return 13
+eight(minus(three())) # must return 5
+six(divided_by(two())) # must return 3
+
+Requirements:
+
+    There must be a function for each number from 0 ("zero") to 9 ("nine")
+    There must be a function for each of the following mathematical operations: plus, minus, times, divided_by
+    Each calculation consist of exactly one operation and two numbers
+    The most outer function represents the left operand, the most inner function represents the right operand
+    Division should be integer division. For example, this should return 2, not 2.666666...:
+
+eight(divided_by(three()))
+"""
+
+
+def self(a): return a
+
+def zero(f=self): return f(0)
+def one(f=self): return f(1)
+def two(f=self): return f(2)
+def three(f=self): return f(3)
+def four(f=self): return f(4)
+def five(f=self): return f(5)
+def six(f=self): return f(6)
+def seven(f=self): return f(7)
+def eight(f=self): return f(8)
+def nine(f=self): return f(9)
+
+# return lambda a: a + b
+# returns a lambda function 
+# for example if b = one() and plus
+# it returns a:a + one()
+
+def plus(b): return lambda a: a + b
+def minus(b): return lambda a: a - b
+def times(b): return lambda a: a * b
+def divided_by(b): return lambda a: a // b
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+    def test_calculation(self):
+        self.assertEqual(seven(times(five())), 35)
+        self.assertEqual(four(plus(nine())), 13)
+        self.assertEqual(eight(minus(three())), 5)
+        self.assertEqual(six(divided_by(two())), 3)
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ python_wars/check_if_number_is_prime.py
@@ -0,0 +1,54 @@
+﻿"""
+Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+Requirements
+
+    You can assume you will be given an integer input.
+    You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+    NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 (or similar, depends on language version). Looping all the way up to n, or n/2, will be too slow.
+
+Example
+
+is_prime(1)  /* false */
+is_prime(2)  /* true  */
+is_prime(-1) /* false */
+"""
+import math 
+import unittest
+
+
+def is_prime(num):
+    if num == 2: return True
+
+    if ((num < 2) or (num % 2 == 0)): 
+        return False
+
+    sqrt_num = math.floor(math.sqrt(num))
+    for divisor in range(3, sqrt_num + 1, 2):
+        if num % divisor == 0:
+            return False
+    return True
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_not_prime(self):
+        self.assertEqual(is_prime(-40), False)
+        self.assertEqual(is_prime(0), False)
+        self.assertEqual(is_prime(1), False)
+        self.assertEqual(is_prime(4), False)
+        self.assertEqual(is_prime(9), False)
+        self.assertEqual(is_prime(121), False)
+
+    def test_prime(self):
+        self.assertEqual(is_prime(2), True)
+        self.assertEqual(is_prime(3), True)
+        self.assertEqual(is_prime(5), True)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
