848a98e0a89d39aab93220a55ad541aaa0e805f2
Lena Bykov <lena.bykov@aristech.de>
2022-06-22 11:34:52 +0200

    chg: add a zip solution

--- python_wars/06_pick_peaks.py
+++ python_wars/06_pick_peaks.py
@@ -92,6 +92,10 @@ def pick_peaks_easy(arr):
             prob_peak = False
     return {'pos':pos, 'peaks':[arr[i] for i in pos]}
 
+def pick_peaks_zip(arr):
+    pos_delta = [pd for pd in enumerate((b - a for a, b in zip(arr, arr[1:])), 1) if pd[1]]
+    positions = [a[0] for a, b in zip(pos_delta, pos_delta[1:]) if a[1] > 0 and b[1] < 0]
+    return {'pos': positions, 'peaks': [arr[p] for p in positions]}
 
 def main():
     unittest.main()
